c37411c3c1246b4310faf7495ea00fd1316481dd
Justin <twoplatinumchainz@gmail.com>
2019-12-09 00:13:34 -0800

    password generator rewritten to work like the demo

--- script.js
+++ script.js
@@ -2,50 +2,61 @@ var specialArr = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',
 var numericArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var lowerArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-
-var userPassword = '';
+var selectedArr = [];
 var userPasswordArr = [];
 var passLength = 0;
+var userPassword = '';
 
 function genPassword() {
     passwordReset();
 
-    do {
-        passLength = parseInt(prompt('Choose a password length between 8 and 128 characters.'));
-    } while (isNaN(passLength) || passLength > 128 || passLength < 8);
+    passLength = prompt('Choose a password length between 8 and 128 characters.');
 
-    do {
-        var charType = prompt('Choose a character type: \'Special\', \'Numeric\', \'Lowercase\', or \'Uppercase\'');
-        var charTypeName = charType.toLowerCase();
-    } while (charTypeName !== 'special' && charTypeName !== 'numeric' && charTypeName !== 'lowercase' && charTypeName !== 'uppercase');
-
-    if (charTypeName === 'special') {
-        var whichArr = specialArr;
-        getArr(whichArr);
-    } else if (charTypeName === 'numeric') {
-        var whichArr = numericArr;
-        getArr(numericArr);
-    } else if (charTypeName === 'lowercase') {
-        var whichArr = lowerArr;
-        getArr(lowerArr);
+    if (parseInt(passLength) < 8 || parseInt(passLength) > 128) {
+        alert('Invalid length. Please choose a length between 8 and 128 characters.');
+    } else if (passLength === null) {
+        alert('A password will not be generated.');
     } else {
-        var whichArr = upperArr;
-        getArr(upperArr);
+        selectedCharacters();
+        console.log(userPassword);
     }
 
     document.getElementById('password-form').textContent = userPassword;
+}
+
+function selectedCharacters() {
+    var specialYes = confirm('Click OK to include special characters in your password.');
+    if (specialYes === true) {
+        selectedArr.push(specialArr);
+    }
+    var numericYes = confirm('Click OK to include numerical characters in your password.');
+    if (numericYes === true) {
+        selectedArr.push(numericArr);
+    }
+    var lowerYes = confirm('Click OK to include lowercase characters in your password.');
+    if (lowerYes === true) {
+        selectedArr.push(lowerArr);
+    } 
+    var upperYes = confirm('Click OK to include uppercase characters in your password.');
+    if (upperYes === true) {
+        selectedArr.push(upperArr);
+    }
+    
+    var merged = [].concat.apply([], selectedArr);
+
+    getArr(merged);
+
+    console.log(userPassword);
 }
 
 function passwordReset() {
     userPasswordArr.length = 0;
 }
 
-function getArr(whichArr) {
+function getArr(mergedArr) {
     for (var i = 0; i < passLength; i++) {
-        var randomChar = Math.floor(Math.random() * whichArr.length);
-        userPasswordArr.push(whichArr[randomChar]);
+        var randomChar = Math.floor(Math.random() * mergedArr.length);
+        userPasswordArr.push(mergedArr[randomChar]);
         userPassword = userPasswordArr.join('');
     }
 }
-
-console.log(userPassword);
